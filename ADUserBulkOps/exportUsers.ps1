5f54cfe2b6d381cfacffadb34c20b185509fe10f
Ashwani K <36263233+AgileAshwani@users.noreply.github.com>
2021-05-05 03:08:10 +0530

    Add files via upload

---
+++ ADUserBulkOps/exportUsers.ps1
@@ -0,0 +1,21 @@
+﻿
+ function UserExport ($usrname){
+                    
+                        $user = get-aduser $usrname -properties samaccountname,givenname,surname
+                        $groups = Get-ADPrincipalGroupMembership $usrname | select -expand name
+                        $data = [PSCustomObject]@{
+                                  samaccountname = $user.samaccountname;
+                                  givenname = $user.givenname;
+                                  surname = $user.surname;
+                                  memberOf = ($groups -join ',' )
+                                }
+                        $data  | Export-Csv .\UserInfo.csv -Delimiter ";" -NoTypeInformation -Encoding UTF8 -Append
+                     }
+
+$adusers = Get-ADUser -Filter * -SearchBase "OU=Tesco,DC=CORPTEST,DC=AD" | Select-Object samaccountname
+
+foreach($aduser in $adusers){
+
+    UserExport -usrname $aduser.samaccountname
+    
+}
